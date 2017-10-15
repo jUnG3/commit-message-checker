38412ed9bb448b817b39d603303cf0ba6280bff7
jUnG3 <soldo.stjepan1304@gmail.com>
2017-10-15 14:09:29 +0200

    Adds main unit test

--- src/test/java/de/java/commit/MainTest.java
+++ src/test/java/de/java/commit/MainTest.java
@@ -1,5 +1,19 @@
 package de.java.commit;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class MainTest {
+
+    @Test
+    public void filterCommentedLines_lineNotCommented_expectsTrue() {
+        assertEquals(true, Main.filterCommentedLines("Line is not comment"));
+    }
+
+    @Test
+    public void filterCommentedLines_lineCommented_expectsFalse() {
+        assertEquals(false, Main.filterCommentedLines("#Line is commented"));
+    }
 
 }
